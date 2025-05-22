14715ea394e434b93d18f8bd3b76cf3f92bc91bd
vals <valentina.derespinis@gmail.com>
2025-05-22 18:39:29 +0200

    deleted old backend and others minor fixes

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI() #creates a server
 
-app.add_middleware( #va qui?
+app.add_middleware( 
   CORSMiddleware,
-  #allow_origins=["http://localhost:5173","http://localhost:3000"],  # porta React
-  allow_origins=["*"],  # porta React TEST PURPOSES, DA ELIMINARE
+  allow_origins=["http://localhost:5173","http://localhost:3000"],  # porta React
   #allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
