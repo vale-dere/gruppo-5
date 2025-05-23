5c37f104f76121bddb186cb555ca91260c5c517a
daanyss <danila.meleleo@gmail.com>
2025-05-23 08:55:58 +0200

    Frontend updated - button upload dataset

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/ButtonUpload.css'; // Riutilizza gli stili già usati
 
 export default function FileUpload({ selectedFile, onFileChange }) {
   return (
@@ -36,24 +37,27 @@ export default function FileUpload({ selectedFile, onFileChange }) {
       </label>
       */}
 
-      <label className="upload-button">
+      <label className="anonymize-button" style={{ cursor: 'pointer' }} >
       <input
         type="file"
         accept=".csv, .json"
         onChange={onFileChange}
         style={{ display: 'none' }}
       />
-      <span className="custom-button">
-        <svg mlns="http://www.w3.org/2000/svg"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                viewBox="0 0 24 24" />
-        Upload dataset
-      </span>
+        <svg 
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              viewBox="0 0 24 24" 
+              width="20"
+              height="20" >
+              <path d="M4 4v16h16V4H4zm8 12v-4M8 12l4-4 4 4" />
+              </svg>
+        {selectedFile ? "Change File" : "Upload Dataset"}
     </label>
-    </div>
+  </div>
   );
 }
